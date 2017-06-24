f2caac7ccd5f7c34a70b7f3ecca015ec605e4fad
jing <1437060911@qq.com>
2017-06-24 15:31:49 +0800

    scientific action

---
+++ src/main/java/com/wncg/service/ISientificService.java
@@ -0,0 +1,10 @@
+package com.wncg.service;
+
+import com.wncg.pojo.ScientificResearch;
+
+/**
+ * Created by jing on 2017/6/20.
+ */
+public interface ISientificService {
+    void addScientific(ScientificResearch scientificResearch);
+}
